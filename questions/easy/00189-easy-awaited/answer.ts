d8cb170f90bfcc5a97783b8287dcd23394527497
lovezhangchuangxin <2911331070@qq.com>
2024-07-23 23:38:47 +0800

    feat: MyAwaited

---
+++ questions/easy/00189-easy-awaited/answer.ts
@@ -0,0 +1,8 @@
+type MyAwaited<T extends PromiseLike<any>> =
+    T extends PromiseLike<infer U>
+        ? U extends PromiseLike<any>
+            ? MyAwaited<U>
+            : U
+        : never
+
+export { MyAwaited }
