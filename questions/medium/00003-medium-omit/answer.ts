4b2c9cafad4194e3a9698c5fcf7e3ec3eac03681
lovezhangchuangxin <2911331070@qq.com>
2024-07-25 22:12:43 +0800

    feat: MyOmit

---
+++ questions/medium/00003-medium-omit/answer.ts
@@ -0,0 +1,7 @@
+type MyOmit<T, K extends keyof T> = {
+    [P in keyof T as P extends K ? never : P]: T[P]
+}
+
+// type MyOmit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+
+export { MyOmit }
